5f512e597106b065262422b6bb35fba1ef2caee1
Reza Nikfal <mr.nikfal@gmail.com>
2020-06-21 12:00:33 -0500

    Leaflet Boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import * as L from 'Leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'leafletSetUp';
+  ngOnInit() {
+    var map = L.map('map', {
+      center: [40, 0],
+      zoom: 3,
+    });
+
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+    }).addTo(map);
+  }
 }
